642af1ee135348a12662ca5819b4f9ecb273dea6
Vinícius Jabes <vinijabes@gmail.com>
2020-05-16 23:42:33 -0300

    added object wrapper

---
+++ pkg/gstreamer/object.c
@@ -0,0 +1,42 @@
+#include "object.h"
+
+void gostreamer_object_set_string(GstObject* object,const char* first_property_name, gchar* arg){
+    g_object_set(object, first_property_name, arg, NULL);
+}
+
+void gostreamer_object_set_int(GstObject* object,const char* first_property_name, gint arg){
+    gint64 garg = arg;
+    g_object_set(object, first_property_name, garg, NULL);
+}
+
+void gostreamer_object_set_uint(GstObject* object,const char* first_property_name, guint arg){
+    g_object_set(object, first_property_name, arg, NULL);
+}
+
+void gostreamer_object_set_bool(GstObject* object, const char* first_property_name, gboolean arg){
+    g_object_set(object, first_property_name, arg, NULL);
+}
+
+gchar* gostreamer_object_get_string(GstObject* object, const char* first_property_name) {
+    gchar *data;
+    g_object_get (object, first_property_name, &data, NULL);
+    return data;
+}
+
+gint gostreamer_object_get_int(GstObject* object, const char* first_property_name) {
+    gint data;
+    g_object_get (object, first_property_name, &data, NULL);
+    return data;
+}
+
+guint gostreamer_object_get_uint(GstObject* object, const char* first_property_name){
+    guint data;
+    g_object_get (object, first_property_name, &data, NULL);
+    return data;
+}
+
+gboolean gostreamer_object_get_bool(GstObject* object, const char* first_property_name){
+    gboolean data;
+    g_object_get (object, first_property_name, &data, NULL);
+    return data;
+}
---
+++ pkg/gstreamer/object.go
@@ -0,0 +1,115 @@
+package gstreamer
+
+/*
+#cgo pkg-config: gstreamer-1.0
+#include "../../include/object.h"
+*/
+import "C"
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+//Object is a GSTObject wrapper interface
+type Object interface {
+	GetParent() Object
+	Unref()
+
+	SetName(string)
+	GetName() string
+
+	GetObjectPointer() *C.GstObject
+
+	Set(string, interface{})
+	Get(string, interface{})
+}
+
+//object ...
+type object struct {
+	GstObject *C.GstObject
+}
+
+func (o *object) GetParent() Object {
+	parent := C.gst_object_get_parent(o.GstObject)
+	if parent != nil {
+		newObj := &object{
+			GstObject: parent,
+		}
+
+		return newObj
+	}
+
+	return nil
+}
+
+func (o *object) Unref() {
+	C.gst_object_unref(C.gpointer(unsafe.Pointer(o.GstObject)))
+}
+
+func (o *object) GetName() (name string) {
+	n := C.gst_object_get_name(o.GstObject)
+
+	if n != nil {
+		name = C.GoString(n)
+	}
+
+	return
+}
+
+func (o *object) SetName(name string) {
+	cname := C.CString(name)
+	defer C.free(unsafe.Pointer(cname))
+
+	C.gst_object_set_name(o.GstObject, cname)
+}
+
+func (o *object) GetObjectPointer() *C.GstObject {
+	return o.GstObject
+}
+
+func (o *object) Set(name string, value interface{}) {
+	cname := (*C.gchar)(unsafe.Pointer(C.CString(name)))
+	defer C.g_free(C.gpointer(unsafe.Pointer(cname)))
+
+	switch value.(type) {
+	case string:
+		str := (*C.gchar)(unsafe.Pointer(C.CString(value.(string))))
+		defer C.g_free(C.gpointer(unsafe.Pointer(str)))
+		C.gostreamer_object_set_string(o.GstObject, cname, str)
+	case int:
+		C.gostreamer_object_set_int(o.GstObject, cname, C.gint(value.(int)))
+	case uint32:
+		C.gostreamer_object_set_uint(o.GstObject, cname, C.guint(value.(uint32)))
+	case bool:
+		var cvalue int
+		if value.(bool) == true {
+			cvalue = 1
+		} else {
+			cvalue = 0
+		}
+		C.gostreamer_object_set_bool(o.GstObject, cname, C.gboolean(cvalue))
+	}
+}
+
+func (o *object) Get(name string, value interface{}) {
+	cname := (*C.gchar)(unsafe.Pointer(C.CString(name)))
+	defer C.g_free(C.gpointer(unsafe.Pointer(cname)))
+
+	switch value.(type) {
+	case *string:
+		*(value.(*string)) = C.GoString(C.gostreamer_object_get_string(o.GstObject, cname))
+	case *int:
+		*(value.(*int)) = int(C.gostreamer_object_get_int(o.GstObject, cname))
+	case *uint32:
+		*(value.(*uint32)) = uint32(C.gostreamer_object_get_uint(o.GstObject, cname))
+	case *bool:
+		*(value.(*bool)) = !(int(C.gostreamer_object_get_bool(o.GstObject, cname)) == 0)
+	default:
+		fmt.Printf("Type not found %s", reflect.TypeOf(value))
+	}
+}
+
+func convertPointerToObject(pointer unsafe.Pointer) *C.GstObject {
+	return (*C.GstObject)(pointer)
+}
