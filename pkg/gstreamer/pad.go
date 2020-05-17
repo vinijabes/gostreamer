c1fe43b0500f06cbeda0a3c07fa92dc2ef9873c7
Vinícius Jabes <vinijabes@gmail.com>
2020-05-17 18:56:56 -0300

    added pad wrapper support

---
+++ pkg/gstreamer/pad.go
@@ -0,0 +1,92 @@
+package gstreamer
+
+/*
+#cgo pkg-config: gstreamer-1.0
+#include "../../include/gostreamer.h"
+*/
+import "C"
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type Pad interface {
+	Object
+}
+
+type pad struct {
+	object
+}
+
+type PadTemplate interface {
+	Object
+}
+
+type padTemplate struct {
+	object
+}
+
+type GstPadDirection int
+type GstPadPresence int
+
+const (
+	GstPadUnknown GstPadDirection = iota
+	GstPadSrc
+	GstPadSink
+)
+
+const (
+	GstPadAlways GstPadPresence = iota
+	GstPadSometimes
+	GstPadRequest
+)
+
+func NewPad(name *string, direction GstPadDirection) (Pad, error) {
+	pad := &pad{}
+
+	if name == nil {
+		cpad := C.gst_pad_new(nil, C.GstPadDirection(direction))
+		if cpad == nil {
+			return nil, fmt.Errorf("failed to create Pad")
+		}
+
+		pad.GstObject = convertPointerToObject(unsafe.Pointer(cpad))
+	} else {
+		cname := C.CString(*name)
+		defer C.free(unsafe.Pointer(cname))
+
+		cpad := C.gst_pad_new(nil, C.GstPadDirection(direction))
+		if cpad == nil {
+			return nil, fmt.Errorf("failed to create Pad")
+		}
+		pad.GstObject = convertPointerToObject(unsafe.Pointer(cpad))
+	}
+
+	return pad, nil
+}
+
+func NewPadTemplate(name *string, direction GstPadDirection, presence GstPadPresence, caps Caps) (PadTemplate, error) {
+	padTemplate := &padTemplate{}
+
+	if name == nil {
+		cpadTemplate := C.gst_pad_template_new(nil, C.GstPadDirection(direction), C.GstPadPresence(presence), caps.GetCapsPointer())
+		if cpadTemplate == nil {
+			return nil, fmt.Errorf("failed to create PadTemplate")
+		}
+
+		padTemplate.GstObject = convertPointerToObject(unsafe.Pointer(cpadTemplate))
+	} else {
+		cpadName := C.CString(*name)
+		defer C.free(unsafe.Pointer(cpadName))
+
+		cpadTemplate := C.gst_pad_template_new(cpadName, C.GstPadDirection(direction), C.GstPadPresence(presence), caps.GetCapsPointer())
+		if cpadTemplate == nil {
+			return nil, fmt.Errorf("failed to create PadTemplate")
+		}
+
+		padTemplate.GstObject = convertPointerToObject(unsafe.Pointer(cpadTemplate))
+	}
+
+	return padTemplate, nil
+}
---
+++ test/pad_test.go
@@ -0,0 +1,31 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/vinijabes/gostreamer/pkg/gstreamer"
+)
+
+func TestNewPad(t *testing.T) {
+	t.Run("NewUnknownDirectionPad", func(t *testing.T) {
+		name := "name"
+		_, err := gstreamer.NewPad(&name, gstreamer.GstPadUnknown)
+		ok(t, err)
+	})
+
+	t.Run("NewSrcDirectionPad", func(t *testing.T) {
+		name := "name"
+		_, err := gstreamer.NewPad(&name, gstreamer.GstPadSrc)
+		ok(t, err)
+	})
+
+	t.Run("NewSinkDirectionPad", func(t *testing.T) {
+		name := "name"
+		_, err := gstreamer.NewPad(&name, gstreamer.GstPadSink)
+		ok(t, err)
+	})
+}
+
+func TestPad(t *testing.T) {
+
+}
